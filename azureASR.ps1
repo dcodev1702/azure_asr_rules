6ac0a081e50426620d7753f954dddda960e3a96a
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2022-12-16 23:15:26 -0500

    Fixed logic for ARC Servers from 'running' to 'Connected'

--- azureASR.ps1
+++ azureASR.ps1
@@ -4,7 +4,7 @@
   - Block = 1
   - Audit = 2
   - Warn = 6
-
+  
   Author: Lorenzo J. Ireland
   Date: 16 Dec 2022
 ########################################################>
@@ -24,28 +24,29 @@ function Enable-ASR {
     Param (
         [Parameter(Mandatory = $true)] [String]$ResourceGroup,
         [Parameter(Mandatory = $true)] [ValidateSet(0,1,2,6)] [int]$Mode,
-        [Parameter(Mandatory = $true)] [String]$VirtualMachine 
+        [Parameter(Mandatory = $false)] [String]$VirtualMachine,
+        [Parameter(Mandatory = $false)] [Switch]$All = $false
     )
 
     Begin {
 
         $asr_rules = @(
-          '56a863a9-875e-4185-98a7-b882c64b5ce5', # Block abuse of vuln signed drivers
-          '7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c', # Block Adobe Reader from creating child processes
-          'd4f940ab-401b-4efc-aadc-ad5f3c50688a', # Block all Office apps from creating child processes
-          '9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2', # Block credential stealing from LSASS.EXE
-          'be9ba2d9-53ea-4cdc-84e5-9b1eeee46550', # Block executable content from email client & webmail
-          '01443614-cd74-433a-b99e-2ecdc07bfc25', # Block executable files from running unless they meet criteria
-          '5beb7efe-fd9a-4556-801d-275e5ffc04cc', # Block execution of potentially obfuscated scripts
-          'd3e037e1-3eb8-44c8-a917-57927947596d', # Block Javascript | VBScript from launching downloaded executable content
-          '3b576869-a4ec-4529-8536-b80a7769e899', # Block Office apps from creating executable content
-          '75668c1f-73b5-4cf0-bb93-3ecf5cb7cc84', # Block Office apps from injecting code into other processes
-          '26190899-1602-49e8-8b27-eb1d0a1ce869', # Block Office communication application from creating child processes
-          'e6db77e5-3df2-4cf1-b95a-636979351e5b', # Block persistence through WMI event subscription
-          'd1e49aac-8f56-4280-b9ba-993a6d77406c', # Block process creations originating from PSExec & WMI commands
-          'b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4', # Block untrusted & unsigned processes that run from USB
-          '92e97fa1-2edf-4476-bdd6-9dd0b4dddc7b', # Block Win32 API calls from Office macros
-          'c1db55ab-c21a-4637-bb3f-a12568109d35'  # Use advanced protection against ransomware
+            '56a863a9-875e-4185-98a7-b882c64b5ce5', # Block abuse of vuln signed drivers
+            '7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c', # Block Adobe Reader from creating child processes
+            'd4f940ab-401b-4efc-aadc-ad5f3c50688a', # Block all Office apps from creating child processes
+            '9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2', # Block credential stealing from LSASS.EXE
+            'be9ba2d9-53ea-4cdc-84e5-9b1eeee46550', # Block executable content from email client & webmail
+            '01443614-cd74-433a-b99e-2ecdc07bfc25', # Block executable files from running unless they meet criteria
+            '5beb7efe-fd9a-4556-801d-275e5ffc04cc', # Block execution of potentially obfuscated scripts
+            'd3e037e1-3eb8-44c8-a917-57927947596d', # Block Javascript | VBScript from launching downloaded executable content
+            '3b576869-a4ec-4529-8536-b80a7769e899', # Block Office apps from creating executable content
+            '75668c1f-73b5-4cf0-bb93-3ecf5cb7cc84', # Block Office apps from injecting code into other processes
+            '26190899-1602-49e8-8b27-eb1d0a1ce869', # Block Office communication application from creating child processes
+            'e6db77e5-3df2-4cf1-b95a-636979351e5b', # Block persistence through WMI event subscription
+            'd1e49aac-8f56-4280-b9ba-993a6d77406c', # Block process creations originating from PSExec & WMI commands
+            'b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4', # Block untrusted & unsigned processes that run from USB
+            '92e97fa1-2edf-4476-bdd6-9dd0b4dddc7b', # Block Win32 API calls from Office macros
+            'c1db55ab-c21a-4637-bb3f-a12568109d35'  # Use advanced protection against ransomware
         )
 
         $ModeType = ""
@@ -56,7 +57,11 @@ function Enable-ASR {
             6 { $ModeType = "Warn" }
         }
 
-        $azure_vms = Get-AzVM -Status
+        if ($All) {
+            $azure_vms = Get-AzVM -Status
+            $arc_vms = Get-AzConnectedMachine
+        }
+        
     }
     
     Process {
@@ -69,11 +74,11 @@ function Enable-ASR {
         }
         
         #$arc_vms = Get-AzConnectedMachine
-        #$arc_vms | ForEach-Object {
-        #    if($_.GuestConfigurationServiceStatus -eq 'running' -and $_.OsType -eq 'windows') {
-        #        Write-Output "Azure ARC VM: $_.Name"
-        #    }
-        #}
+        $arc_vms | ForEach-Object {
+            if($_.Status -eq 'Connected' -and $_.OsType -eq 'windows') {
+                Write-Output "Azure ARC VM:$($_.Name)"
+            }
+        }
 
     }
 
