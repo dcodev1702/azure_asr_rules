4c930a0afe82d460825b1e0750911fcb83b097d9
DCO DEV <zo@mail.com>
2023-01-05 17:05:15 -0500

    Converted ASRRules to JSON and added the ability to pull ASR Rules locally or remotely.

--- azureASR.ps1
+++ azureASR.ps1
@@ -128,10 +128,21 @@ function Set-ASRRules {
     )
 
     Begin {
-    
-        $asrRuleFile = 'asr_rules.txt'
-        $asr_rule_file = "$PSScriptRoot\$asrRuleFile"
 
+        # List of ASR Rules - Dated 18 DEC 2022
+        # https://github.com/MicrosoftDocs/microsoft-365-docs/blob/public/microsoft-365/security/defender-endpoint/attack-surface-reduction-rules-reference.md
+        
+        # Inspired from: https://github.com/Kaidja/Defender-for-Endpoint
+
+        # The option exists to pull the ASR Rules locally or remotely via GitHub
+    
+        # Attack Surface Reduction Rules JSON File
+        #$URL = "https://raw.githubusercontent.com/dcodev1702/azure_asr_rules/main/AttackSurfaceReductionRules.json"
+        #$ASRRules = (Invoke-WebRequest -Uri $URL -UseBasicParsing).Content | ConvertFrom-Json
+        
+        $ASRRules = Get-Content -Raw ./AttackSurfaceReductionRules.json | ConvertFrom-Json
+        
+        
         # Use a flag -CheckAzModules to enable checking of required modules
         if ($CheckAzModules) { Check-AzModules }
 
@@ -149,36 +160,6 @@ function Set-ASRRules {
             Write-Host "[1] Resource Group:[$ResourceGroup] successfully located..." -ForegroundColor Green
         }
 
-        # List of ASR Rules - Dated 18 DEC 2022
-        # https://github.com/MicrosoftDocs/microsoft-365-docs/blob/public/microsoft-365/security/defender-endpoint/attack-surface-reduction-rules-reference.md
-        <#
-        $asr_rules = @(
-            '56a863a9-875e-4185-98a7-b882c64b5ce5', # Block abuse of vuln signed drivers
-            '7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c', # Block Adobe Reader from creating child processes
-            'd4f940ab-401b-4efc-aadc-ad5f3c50688a', # Block all Office apps from creating child processes
-            '9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2', # Block credential stealing from LSASS.EXE
-            'be9ba2d9-53ea-4cdc-84e5-9b1eeee46550', # Block executable content from email client & webmail
-            '01443614-cd74-433a-b99e-2ecdc07bfc25', # Block executable files from running unless they meet criteria
-            '5beb7efe-fd9a-4556-801d-275e5ffc04cc', # Block execution of potentially obfuscated scripts
-            'd3e037e1-3eb8-44c8-a917-57927947596d', # Block Javascript | VBScript from launching downloaded executable content
-            '3b576869-a4ec-4529-8536-b80a7769e899', # Block Office apps from creating executable content
-            '75668c1f-73b5-4cf0-bb93-3ecf5cb7cc84', # Block Office apps from injecting code into other processes
-            '26190899-1602-49e8-8b27-eb1d0a1ce869', # Block Office communication application from creating child processes
-            'e6db77e5-3df2-4cf1-b95a-636979351e5b', # Block persistence through WMI event subscription
-            'd1e49aac-8f56-4280-b9ba-993a6d77406c', # Block process creations originating from PSExec & WMI commands
-            'b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4', # Block untrusted & unsigned processes that run from USB
-            '92e97fa1-2edf-4476-bdd6-9dd0b4dddc7b', # Block Win32 API calls from Office macros
-            'c1db55ab-c21a-4637-bb3f-a12568109d35'  # Use advanced protection against ransomware
-        )
-        #>
-        
-        # Obtain current ASR rules from a file and store in an array (asr_rules)
-        $asr_rules = @()
-        if (Test-Path -Path $asr_rule_file) {
-            $asr_rules_from_file = [System.IO.File]::ReadAllLines($asr_rule_file)
-            
-            $asr_rules_from_file | ForEach-Object { $asr_rules += $_.Split(',')[0] }            
-        }
 
         # Validate input recieved and quit the program if a bad value is provided.
         [bool]$VMEnabled = $false
@@ -190,15 +171,16 @@ function Set-ASRRules {
             6 { $ModeType = "Warn" }
         }
 
-        # If specific rules have been provided, validate them before proceeding
-        $tmpRules = @()
-        $tmpRules = $Rule.Split(',')
-
+        
         if (-not ([String]::IsNullOrEmpty($Rule))) {
 
+            # If specific rules have been provided, validate them before proceeding
+            $tmpRules = @()
+            $tmpRules = $Rule.Split(',')
+            
             $tmpRules | Where-Object -FilterScript { 
                 
-                if($_ -notin $asr_rules) { 
+                if($_ -notin $ASRRules.GUID) { 
                     Write-Host "`nASR Rule:[$_] - not found, goodbye" -ForegroundColor Red
                     break
                 } else {
