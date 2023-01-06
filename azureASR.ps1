c273ce9fccd56b9b8eb4128f49c904ae03580cea
DCO DEV <zo@mail.com>
2023-01-06 18:44:47 -0500

    Added minor exception handling for Invoke-WebRequest

--- azureASR.ps1
+++ azureASR.ps1
@@ -137,14 +137,26 @@ function Set-ASRRules {
 
         # If access to GitHub is permitted, pull from repo, else pull the ASR Rules locally
         # Attack Surface Reduction Rules JSON File
-        $URL = "https://raw.githubusercontent.com/dcodev1702/azure_asr_rules/main/AttackSurfaceReductionRules.json"
-        $ASRWebReq = Invoke-WebRequest -Uri $URL -UseBasicParsing
-        if ($ASRWebReq.StatusCode -eq 200) {
-            $ASRRules = $ASRWebReq.Content | ConvertFrom-Json
-        } else {
+        $URL = "https://raw.githubusercontent.com/dcodev1702/azure_asr_rules/main/AttackSurfaceReductionRuless.json"
+        #ensure we get a response even if an error's returned
+        $ASRWebReq = try { 
+            (Invoke-WebRequest -Uri $URL -UseBasicParsing -ErrorAction SilentlyContinue).BaseResponse
+        } catch [System.Net.WebException] {
             # GitHub is inaccessible, acquire ASR Rules (JSON format) locally.
+            Write-Host "Web Request to GitHub repo failed, switching to locally defined ASR Rules!`n" -ForegroundColor Yellow
             $ASRRules = Get-Content -Raw ./AttackSurfaceReductionRules.json | ConvertFrom-Json
-        }
+
+            #Write-Error "An exception was caught: $($_.Exception.Message)"
+            #$_.Exception.Response 
+        } 
+        #$ASRWebReq = Invoke-WebRequest -Uri $URL -UseBasicParsing -ErrorAction SilentlyContinue | Out-Null
+        #if ($ASRWebReq.StatusCode -eq 200) {
+        #    $ASRRules = $ASRWebReq.Content | ConvertFrom-Json
+        #} else {
+            # GitHub is inaccessible, acquire ASR Rules (JSON format) locally.
+        #    Write-Host "Web Request to GitHub repo failed, switching to locally defined ASR Rules!`n" -ForegroundColor Yellow
+        #    $ASRRules = Get-Content -Raw ./AttackSurfaceReductionRules.json | ConvertFrom-Json
+        #}
         
         
         # Use a flag -CheckAzModules to enable checking of required modules
